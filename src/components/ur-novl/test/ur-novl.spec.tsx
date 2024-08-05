dd15a4f21081a8e5f96d85cd27ee43274386992c
hellish <hellish.files@gmail.com>
2024-08-05 22:29:47 +0300

    Fix test

--- src/components/ur-novl/test/ur-novl.spec.tsx
+++ src/components/ur-novl/test/ur-novl.spec.tsx
@@ -11,12 +11,36 @@ describe('ur-novl', () => {
         expect(page.root).toEqualHtml(`
             <ur-novl>
                 <mock:shadow-root>
-                    <section class="cover"></section>
+                    <section class="cover" style="background-image: url(undefined);"></section>
                     <section class="info">
                         <div class="title">
                             Novl title
                         </div>
-                    </section>
+                        <div class="stats">
+                            <div class="likes">
+                            <div>
+                                <b>
+                                    0
+                                </b>
+                                Likes
+                            </div>
+                            <div class="dot">
+                                ·
+                            </div>
+                        </div>
+                        <div class="views">
+                            <b>
+                                0
+                            </b>
+                            Views
+                        </div>
+                    </div>
+                    <div class="description"></div>
+                    <div class="author">
+                        <div class="avatar" style="background-image: url(undefined);"></div>
+                        <div class="name"></div>
+                    </div>
+                   </section>
                 </mock:shadow-root>
             </ur-novl>
         `);
