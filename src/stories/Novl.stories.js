e206a1d2b8679a34588fff861c47abee051576c1
hellish <hellish.files@gmail.com>
2024-08-06 20:51:20 +0300

    Add full example with everything

--- src/stories/Novl.stories.js
+++ src/stories/Novl.stories.js
@@ -77,3 +77,19 @@ export const WithPrice = {
     price: '15$',
   },
 };
+
+export const Full = {
+  args: {
+    novlTitle: "But you can't help, this is the end Of a tale that wasn't right",
+    novlCover: 'https://s3-eu-west-1.amazonaws.com/urnovl-irl-1/covers/fe469e2a-8e19-48a2-a89d-55e8a201156c-grid.jpg',
+    likes: 7,
+    views: 1024,
+    showStats: true,
+    novlDescription:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    authorAvatar: 'https://lh4.googleusercontent.com/-FxnW9K6NrJY/AAAAAAAAAAI/AAAAAAAAACM/irHh3OywY_Y/photo.jpg',
+    authorName: 'Jesus Christ Super Star 🌟',
+    published: false,
+    price: '1555€',
+  },
+};
