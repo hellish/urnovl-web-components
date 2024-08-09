399ef2aa93289a3f0732efaf5f03135c9f16041d
hellish <hellish.files@gmail.com>
2024-08-09 14:05:35 +0300

    Lint

--- src/components/ur-novl-carousel/test/ur-novl-carousel.e2e.ts
+++ src/components/ur-novl-carousel/test/ur-novl-carousel.e2e.ts
@@ -1,11 +1,10 @@
 import { newE2EPage } from '@stencil/core/testing';
 
 describe('ur-novl-carousel', () => {
-  it('renders', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<ur-novl-carousel></ur-novl-carousel>');
-
-    const element = await page.find('ur-novl-carousel');
-    expect(element).toHaveClass('hydrated');
-  });
+    it('renders', async () => {
+        const page = await newE2EPage();
+        await page.setContent('<ur-novl-carousel></ur-novl-carousel>');
+        const element = await page.find('ur-novl-carousel');
+        expect(element).toHaveClass('hydrated');
+    });
 });
--- src/components/ur-novl/test/ur-novl.e2e.ts
+++ src/components/ur-novl/test/ur-novl.e2e.ts
@@ -1,11 +1,10 @@
 import { newE2EPage } from '@stencil/core/testing';
 
 describe('ur-novl', () => {
-  it('renders', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<ur-novl></ur-novl>');
-
-    const element = await page.find('ur-novl');
-    expect(element).toHaveClass('hydrated');
-  });
+    it('renders', async () => {
+        const page = await newE2EPage();
+        await page.setContent('<ur-novl></ur-novl>');
+        const element = await page.find('ur-novl');
+        expect(element).toHaveClass('hydrated');
+    });
 });
