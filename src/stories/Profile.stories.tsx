f7738fa21d3831f68f2b5f550e8359fe63d816f6
hellish <hellish.files@gmail.com>
2024-09-03 17:04:53 +0300

    One more story

--- src/stories/Profile.stories.tsx
+++ src/stories/Profile.stories.tsx
@@ -63,6 +63,27 @@ export const Default = {
     }),
 };
 
+export const SmallerWidth = {
+    render: () => Profile({
+        name: 'Penguin Publishing',
+        description: 'Passionate writer crafting stories that captivate and inspire. Lover of words, exploring the world one page at a time. Always seeking new tales to tell.',
+        location: 'London, UK',
+        facebook_url: 'https://facebook.com',
+        twitter_url: 'https://twitter.com',
+        linkedin_url: 'https://linkedin.com',
+        phone: '999666333',
+        email: 'urnovl@gmail.com',
+        website: 'urnovl.co',
+        show_follow: true,
+        show_donate: true,
+        show_become_member: true,
+        show_send_message: true,
+        show_stats: true,
+        languages: [ 'English', 'Greek' ].join(','),
+        width: '200px'
+    }),
+};
+
 export const BiggerWidth = {
     render: () => Profile({
         name: 'Penguin Publishing',
