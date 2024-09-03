deafc98394df2c7e0b48fb3412804d3514122795
hellish <hellish.files@gmail.com>
2024-09-03 16:53:38 +0300

    Updates

--- src/components/ur-button-arrow-left/ur-button-arrow-left.tsx
+++ src/components/ur-button-arrow-left/ur-button-arrow-left.tsx
@@ -1,5 +1,7 @@
 import { Component, Event, EventEmitter, Host, h } from '@stencil/core';
 
+import 'mdui/components/button-icon';
+
 @Component({
     tag: 'ur-button-arrow-left',
     styleUrl: 'ur-button-arrow-left.css',
@@ -13,14 +15,14 @@ export class UrButtonArrowLeft {
     render() {
         return (
             <Host>
-                <button onClick={() => this.leftClicked.emit()}>
+                <mdui-button-icon onClick={() => this.leftClicked.emit()}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="29" height="28" viewBox="0 0 29 28" fill="none">
                         <g>
                             <path d="M22.6667 13.9998L6.33334 13.9998M6.33334 13.9998L14.5 22.1665M6.33334 13.9998L14.5 5.83317"
                                 stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                         </g>
                     </svg>
-                </button>
+                </mdui-button-icon>
             </Host>
         );
     }
--- src/stories/Buttons.stories.tsx
+++ src/stories/Buttons.stories.tsx
@@ -3,10 +3,16 @@ import '../components/ur-button-arrow-left/ur-button-arrow-left';
 import '../components/ur-button-arrow-right/ur-button-arrow-right';
 
 const ButtonArrowLeft = () => html`
+    <link rel="stylesheet" href="https://unpkg.com/mdui@2/mdui.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
     <ur-button-arrow-left></ur-button-arrow-left>
 `;
 
 const ButtonArrowRight = () => html`
+    <link rel="stylesheet" href="https://unpkg.com/mdui@2/mdui.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
     <ur-button-arrow-right></ur-button-arrow-right>
 `;
 
