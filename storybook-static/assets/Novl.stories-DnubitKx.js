82179b1e39091c1b1a1832f2eeca196fb7a58e18
hellish <hellish.files@gmail.com>
2024-10-01 10:47:10 +0300

    Rebuild

--- storybook-static/assets/Novl.stories-DnubitKx.js
+++ storybook-static/assets/Novl.stories-DnubitKx.js
@@ -1,4 +1,4 @@
-import{k as er}from"./entry-preview-BcHUnPw7.js";import{N as r}from"./ur-novl-BhJ-oAOn.js";import"./index-DrFu-skq.js";import"./chunk-L4EGOTBX-2Jzo53v8.js";import"./index-K5CyEtrH.js";const sr=({novlTitle:l,novlCover:A,likes:G,views:H,showStats:I,novlDescription:J,authorAvatar:K,authorName:M,published:Q,price:T,publisherName:X,publisherAvatar:Y,borderRadius:Z,width:rr})=>er`
+import{k as er}from"./entry-preview-BcHUnPw7.js";import{N as r}from"./ur-novl-CyuN1e9V.js";import"./index-DrFu-skq.js";import"./chunk-L4EGOTBX-2Jzo53v8.js";import"./index-K5CyEtrH.js";const sr=({novlTitle:l,novlCover:A,likes:G,views:H,showStats:I,novlDescription:J,authorAvatar:K,authorName:M,published:Q,price:T,publisherName:X,publisherAvatar:Y,borderRadius:Z,width:rr})=>er`
     <style>
         ur-novl {
             --novl-width: ${rr};
@@ -18,7 +18,7 @@ import{k as er}from"./entry-preview-BcHUnPw7.js";import{N as r}from"./ur-novl-Bh
         price="${T}"
         publisher-name="${X}"
         publisher-avatar="${Y}"></ur-novl>
-`,ir={title:"Urnovl/Business/Novl",render:l=>sr(l)},e={args:r[0]},s={args:{...r[0],novlCover:null}},a={args:r[1]},o={args:r[2]},t={args:r[3]},n={args:r[4]},c={args:r[5]},i={args:{...r[6],width:"320px"}},p={args:{...r[7],width:"350px"}},d={args:{...r[7],novlCover:r[6].novlCover,width:"450px",borderRadius:"0"}};var u,m,g;e.parameters={...e.parameters,docs:{...(u=e.parameters)==null?void 0:u.docs,source:{originalSource:`{
+`,ir={title:"Urnovl/Business/Novl",render:l=>sr(l)},e={args:r[0]},s={args:{...r[0],novlCover:null}},a={args:r[1]},o={args:r[2]},t={args:r[3]},n={args:r[4]},c={args:r[5]},i={args:{...r[6],width:"320px"}},p={args:{...r[7],width:"350px"}},d={args:{...r[7],novlCover:r[6].novlCover,width:"100%",borderRadius:"0"}};var u,m,g;e.parameters={...e.parameters,docs:{...(u=e.parameters)==null?void 0:u.docs,source:{originalSource:`{
   args: NOVLS[0]
 }`,...(g=(m=e.parameters)==null?void 0:m.docs)==null?void 0:g.source}}};var v,S,h;s.parameters={...s.parameters,docs:{...(v=s.parameters)==null?void 0:v.docs,source:{originalSource:`{
   args: {
@@ -31,9 +31,9 @@ import{k as er}from"./entry-preview-BcHUnPw7.js";import{N as r}from"./ur-novl-Bh
   args: NOVLS[2]
 }`,...(W=(V=o.parameters)==null?void 0:V.docs)==null?void 0:W.source}}};var w,B,b;t.parameters={...t.parameters,docs:{...(w=t.parameters)==null?void 0:w.docs,source:{originalSource:`{
   args: NOVLS[3]
-}`,...(b=(B=t.parameters)==null?void 0:B.docs)==null?void 0:b.source}}};var x,C,f;n.parameters={...n.parameters,docs:{...(x=n.parameters)==null?void 0:x.docs,source:{originalSource:`{
+}`,...(b=(B=t.parameters)==null?void 0:B.docs)==null?void 0:b.source}}};var C,x,f;n.parameters={...n.parameters,docs:{...(C=n.parameters)==null?void 0:C.docs,source:{originalSource:`{
   args: NOVLS[4]
-}`,...(f=(C=n.parameters)==null?void 0:C.docs)==null?void 0:f.source}}};var k,D,F;c.parameters={...c.parameters,docs:{...(k=c.parameters)==null?void 0:k.docs,source:{originalSource:`{
+}`,...(f=(x=n.parameters)==null?void 0:x.docs)==null?void 0:f.source}}};var k,D,F;c.parameters={...c.parameters,docs:{...(k=c.parameters)==null?void 0:k.docs,source:{originalSource:`{
   args: NOVLS[5]
 }`,...(F=(D=c.parameters)==null?void 0:D.docs)==null?void 0:F.source}}};var P,R,_;i.parameters={...i.parameters,docs:{...(P=i.parameters)==null?void 0:P.docs,source:{originalSource:`{
   args: {
@@ -49,7 +49,7 @@ import{k as er}from"./entry-preview-BcHUnPw7.js";import{N as r}from"./ur-novl-Bh
   args: {
     ...NOVLS[7],
     novlCover: NOVLS[6]['novlCover'],
-    width: '450px',
+    width: '100%',
     borderRadius: '0'
   }
 }`,...(z=(q=d.parameters)==null?void 0:q.docs)==null?void 0:z.source}}};const pr=["Default","BrokenCover","NoStats","SmallDescription","WithPrice","WithPublisher","Full","BiggerWidth","FullWithBiggerWidth","NoBorderRadius"];export{i as BiggerWidth,s as BrokenCover,e as Default,c as Full,p as FullWithBiggerWidth,d as NoBorderRadius,a as NoStats,o as SmallDescription,t as WithPrice,n as WithPublisher,pr as __namedExportsOrder,ir as default};
