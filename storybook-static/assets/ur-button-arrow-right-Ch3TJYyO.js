4af5e6b2c32be4f850df1d511814efb21571e4ff
hellish <hellish.files@gmail.com>
2024-10-06 18:00:07 +0300

    Rebuild

---
+++ storybook-static/assets/ur-button-arrow-right-Ch3TJYyO.js
@@ -0,0 +1,45 @@
+import{s as o,g as e}from"./chunk-L4EGOTBX-2Jzo53v8.js";import"./entry-preview-BcHUnPw7.js";import{d as i,H as s,c as a,h as t,a as d}from"./index-DrtGFqrN.js";import"./index-Co_fSMZb.js";const u=`:host {
+    border-radius: 50%;
+
+    button,
+    mdui-button-icon {
+        height: 34px;
+        width: 34px;
+        border-radius: inherit;
+        fill: #f7f7f7;
+        filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 0;
+        cursor: pointer;
+        background-color: rgb(239, 239, 239);
+    }
+
+    mdui-button-icon svg {
+        height: 21px;
+    }
+}
+`,c=class extends s{constructor(){super(),this.__registerHost(),this.__attachShadow(),this.leftClicked=a(this,"leftClicked",7)}render(){return t(d,{key:"33c0572eeb6af0352953645034eaeedf39ab81bd"},t("mdui-button-icon",{key:"1b6a2342ce8b7eeb0d9ba0156b79448e185783b3",onClick:()=>this.leftClicked.emit()},t("svg",{key:"f42e889d70b324b8d2bbabcb9c3afd3b547e3f6f",xmlns:"http://www.w3.org/2000/svg",width:"29",height:"28",viewBox:"0 0 29 28",fill:"none"},t("g",{key:"2dd381826ec8665c376f1a4c581c897427d01322"},t("path",{key:"6a876c9ceb983f7e0783736d3aaf8af9b47f553c",d:"M22.6667 13.9998L6.33334 13.9998M6.33334 13.9998L14.5 22.1665M6.33334 13.9998L14.5 5.83317",stroke:"#1E1E1E","stroke-width":"2","stroke-linecap":"round","stroke-linejoin":"round"})))))}static get style(){return u}};i(c,[1,"ur-button-arrow-left"]);const n={schemaVersion:"1.0.0",modules:[{kind:"javascript-module",path:"",declarations:[{kind:"class",name:"UrButtonArrowLeft",tagName:"ur-button-arrow-left",customElement:!0,members:[],events:[{kind:"event",name:"leftClicked",description:""}]}],exports:[{kind:"js",name:"UrButtonArrowLeft",declaration:{name:"UrButtonArrowLeft",module:"src/components/ur-button-arrow-left/ur-button-arrow-left.tsx"}},{kind:"custom-element-definition",name:"ur-button-arrow-left",declaration:{name:"UrButtonArrowLeft",module:"src/components/ur-button-arrow-left/ur-button-arrow-left.tsx"}}]}]};o({...e()||{},...n,modules:[...(e()||{}).modules||[],...n.modules]});const l=`:host {
+    border-radius: 50%;
+
+    button,
+    mdui-button-icon {
+        height: 34px;
+        width: 34px;
+        border-radius: inherit;
+        fill: #f7f7f7;
+        filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 0;
+        cursor: pointer;
+        background-color: rgb(239, 239, 239);
+    }
+
+    mdui-button-icon svg {
+        height: 21px;
+    }
+}
+`,m=class extends s{constructor(){super(),this.__registerHost(),this.__attachShadow(),this.rightClicked=a(this,"rightClicked",7)}render(){return t(d,{key:"a3bc72ce3b2b81a4c7367605d462766253430531"},t("mdui-button-icon",{key:"9936fdbcb14323133e61e1b2fd97826cb5498726",onClick:()=>this.rightClicked.emit()},t("svg",{key:"bc5365d45792adc2c7c3778225e67b09752a6247",width:"29",height:"28",viewBox:"0 0 29 28",fill:"none",xmlns:"http://www.w3.org/2000/svg"},t("g",{key:"ad4b2c87f6d67d7f8e3b8f3a1991e4446e944b53",id:"Arrow right"},t("path",{key:"4a55b47c386c6b9117b50116eeec86098d42b58d",id:"Icon",d:"M6.33331 14.0002H22.6666M22.6666 14.0002L14.5 5.8335M22.6666 14.0002L14.5 22.1668",stroke:"#1E1E1E","stroke-width":"2","stroke-linecap":"round","stroke-linejoin":"round"})))))}static get style(){return l}};i(m,[1,"ur-button-arrow-right"]);const r={schemaVersion:"1.0.0",modules:[{kind:"javascript-module",path:"",declarations:[{kind:"class",name:"UrButtonArrowRight",tagName:"ur-button-arrow-right",customElement:!0,members:[],events:[{kind:"event",name:"rightClicked",description:""}]}],exports:[{kind:"js",name:"UrButtonArrowRight",declaration:{name:"UrButtonArrowRight",module:"src/components/ur-button-arrow-right/ur-button-arrow-right.tsx"}},{kind:"custom-element-definition",name:"ur-button-arrow-right",declaration:{name:"UrButtonArrowRight",module:"src/components/ur-button-arrow-right/ur-button-arrow-right.tsx"}}]}]};o({...e()||{},...r,modules:[...(e()||{}).modules||[],...r.modules]});
