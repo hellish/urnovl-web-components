7cb55cb69c8bcc05ce33363638fb33ae32b3720f
hellish <hellish.files@gmail.com>
2024-10-10 19:02:50 +0300

    Testing host

--- src/components/ur-button-arrow-left/ur-button-arrow-left.tsx
+++ src/components/ur-button-arrow-left/ur-button-arrow-left.tsx
@@ -15,14 +15,16 @@ export class UrButtonArrowLeft {
     render() {
         return (
             <Host>
-                <mdui-button-icon onClick={() => this.leftClicked.emit()}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="29" height="28" viewBox="0 0 29 28" fill="none">
-                        <g>
-                            <path d="M22.6667 13.9998L6.33334 13.9998M6.33334 13.9998L14.5 22.1665M6.33334 13.9998L14.5 5.83317"
-                                stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                        </g>
-                    </svg>
-                </mdui-button-icon>
+                <div>
+                    <mdui-button-icon onClick={() => this.leftClicked.emit()}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="29" height="28" viewBox="0 0 29 28" fill="none">
+                            <g>
+                                <path d="M22.6667 13.9998L6.33334 13.9998M6.33334 13.9998L14.5 22.1665M6.33334 13.9998L14.5 5.83317"
+                                    stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            </g>
+                        </svg>
+                    </mdui-button-icon>
+                </div>
             </Host>
         );
     }
