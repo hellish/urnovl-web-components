61a9a7f934058749814e2d7ee59c38d55e5a907f
hellish <hellish.files@gmail.com>
2024-10-20 10:40:34 +0300

    Updates

---
+++ storybook-static/assets/publisher-DuiD-Zl5.js
@@ -0,0 +1,9 @@
+import{s as r,g as e}from"./chunk-L4EGOTBX-2Jzo53v8.js";import"./entry-preview-BcHUnPw7.js";import{d as a,H as i,h as t,a as n}from"./index-CXMOPRVH.js";const o=`:host {
+    img {
+        display: block;
+        border-color: #ececec;
+        border-style: solid;
+        background: #fff;
+    }
+}
+`,d=class extends i{constructor(){super(),this.__registerHost(),this.__attachShadow(),this.size="32px",this.src=void 0,this.name=void 0,this.border="0",this.radius="50%"}render(){return t(n,{key:"647c25b4744b097b7313a1680cc512c085e6d687"},t("img",{key:"122aaaa013b335aa0416bdb780853730c50b3bd8",class:"avatar",src:this.src,style:{borderRadius:this.radius,height:this.size,width:this.size,borderWidth:this.border},alt:this.name}))}static get style(){return o}};a(d,[1,"ur-avatar",{size:[1],src:[1],name:[1],border:[1],radius:[1]}]);const s={schemaVersion:"1.0.0",modules:[{kind:"javascript-module",path:"",declarations:[{kind:"class",name:"UrAvatar",tagName:"ur-avatar",customElement:!0,members:[{kind:"field",name:"size",type:"string",description:"",default:"'32px'"},{kind:"field",name:"src",type:"string",description:""},{kind:"field",name:"name",type:"string",description:""},{kind:"field",name:"border",type:"string",description:"",default:"'0'"},{kind:"field",name:"radius",type:"string",description:"",default:"'50%'"}],events:[]}],exports:[{kind:"js",name:"UrAvatar",declaration:{name:"UrAvatar",module:"src/components/ur-avatar/ur-avatar.tsx"}},{kind:"custom-element-definition",name:"ur-avatar",declaration:{name:"UrAvatar",module:"src/components/ur-avatar/ur-avatar.tsx"}}]}]};r({...e()||{},...s,modules:[...(e()||{}).modules||[],...s.modules]});const u=""+new URL("avatar-l2bw-8_m.svg",import.meta.url).href,p=""+new URL("publisher-pbXrxVc5.svg",import.meta.url).href;export{u as c,p};
