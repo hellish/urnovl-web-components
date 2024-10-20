0d4c592fefc6eca6cbaff611bd47230c8bc578e6
hellish <hellish.files@gmail.com>
2024-10-20 10:42:05 +0300

    Rebuild

---
+++ storybook-static/assets/Checkboxes.stories-Hq4YwwKE.js
@@ -0,0 +1,20 @@
+import{k as _}from"./entry-preview-BcHUnPw7.js";import{s as D,g as t}from"./chunk-L4EGOTBX-2Jzo53v8.js";import{d as E,H as S,h as o,a as U}from"./index-CXMOPRVH.js";import"./index-S3KKWdI2.js";import"./index-DrFu-skq.js";import"./index-CtULszVu.js";import"./form-CYSikw0H.js";import"./live-DYTPoJZU.js";import"./default-value-D_2XsDCq.js";import"./watch-ByvFYHv8.js";import"./svg-tag-CIxfFmRK.js";const H=":host{display:block}",$=class extends S{constructor(){super(),this.__registerHost(),this.__attachShadow(),this.disabled=!1,this.checked=!1}render(){return o(U,{key:"5140a5e63b0090122485ae7600a12a37e643988c"},o("mdui-checkbox",{key:"c2419f935c4a57c628668c85e37b53f09d89497c",part:"check",checked:this.checked,disabled:this.disabled},o("slot",{key:"f80b1a74545bec8e11ceec9805056d32c28465f3"})))}static get style(){return H}};E($,[1,"ur-checkbox",{disabled:[4],checked:[4]}]);const d={schemaVersion:"1.0.0",modules:[{kind:"javascript-module",path:"",declarations:[{kind:"class",name:"UrCheckbox",tagName:"ur-checkbox",customElement:!0,members:[{kind:"field",name:"disabled",type:"boolean",description:"",default:"false"},{kind:"field",name:"checked",type:"boolean",description:"",default:"false"}],events:[]}],exports:[{kind:"js",name:"UrCheckbox",declaration:{name:"UrCheckbox",module:"src/components/ur-checkbox/ur-checkbox.tsx"}},{kind:"custom-element-definition",name:"ur-checkbox",declaration:{name:"UrCheckbox",module:"src/components/ur-checkbox/ur-checkbox.tsx"}}]}]};D({...t()||{},...d,modules:[...(t()||{}).modules||[],...d.modules]});const j=({text:a="Welcome",disabled:C=!1,checked:y=!1})=>_`
+    <ur-checkbox checked=${y} disabled=${C}>${a}</ur-checkbox>
+`,B={title:"Core/Checkboxes",render:a=>j(a),argTypes:{}},e={args:{disabled:!1}},s={args:{checked:!0}},r={args:{disabled:!0}},c={args:{checked:!0,disabled:!0}};var n,i,m;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    disabled: false
+  }
+}`,...(m=(i=e.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var l,u,h;s.parameters={...s.parameters,docs:{...(l=s.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    checked: true
+  }
+}`,...(h=(u=s.parameters)==null?void 0:u.docs)==null?void 0:h.source}}};var k,b,p;r.parameters={...r.parameters,docs:{...(k=r.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...(p=(b=r.parameters)==null?void 0:b.docs)==null?void 0:p.source}}};var f,x,g;c.parameters={...c.parameters,docs:{...(f=c.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    checked: true,
+    disabled: true
+  }
+}`,...(g=(x=c.parameters)==null?void 0:x.docs)==null?void 0:g.source}}};const F=["Default","Checked","Disabled","CheckedAndDisabled"];export{s as Checked,c as CheckedAndDisabled,e as Default,r as Disabled,F as __namedExportsOrder,B as default};
