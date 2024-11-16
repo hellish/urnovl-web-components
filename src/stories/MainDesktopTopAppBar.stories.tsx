ff980df3e10d416e9ba50f8418bfa6040009b5a2
qualitiedx <qualitiedx@gmail.com>
2024-11-16 11:12:26 +0200

    fix overflows

--- src/stories/MainDesktopTopAppBar.stories.tsx
+++ src/stories/MainDesktopTopAppBar.stories.tsx
@@ -4,10 +4,9 @@ import '../components/topappbars/main-desktop-topappbar/ur-main-desktop-topappba
 
 const Template = args => html`
   <ur-main-desktop-topappbar 
-    scroll-behavior="${args.scrollBehavior}"
     search-text="${args.searchText}"></ur-main-desktop-topappbar>
 
-  <div class="example-scroll-target" style="background: #E8E8E8; height: 250px; overflow: auto;">
+  <div class="example-scroll-target" style="background: #F0F0F0; height: 250px; overflow: auto;">
     <div style="height: 1000px;"></div>
   </div>
 `;
@@ -16,11 +15,6 @@ export default {
   title: 'urnovl/Business/TopAppBars/Main Desktop TopAppBar',
   render: Template,
   argTypes: {
-    scrollBehavior: {
-      control: 'select',
-      options: ['hide', 'elevate', 'shrink'], // Available scroll behaviors
-      description: 'Scroll behavior for the top app bar',
-    },
     searchText: {
       control: 'text',
       description: 'Text for the search link',
