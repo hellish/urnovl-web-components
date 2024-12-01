e6d95adc96e17398637730b1aa00028dd91073ed
hellish <hellish.files@gmail.com>
2024-12-01 11:12:20 +0200

    Remove broken test

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,3 @@
-export function format(first: string, middle: string, last: string): string {
-  return (first || '') + (middle ? ` ${middle}` : '') + (last ? ` ${last}` : '');
-}
-
 export const isNumber = (value: any) => {
     return typeof value === 'number' && isFinite(value);
 }
