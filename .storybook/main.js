e879f4ae48304b22ca521e2a4a8bf46e354d4d8e
hellish <hellish.files@gmail.com>
2025-03-23 20:42:00 +0200

    Updates

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,16 +3,11 @@ const config = {
     stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 
     addons: [
+        'storybook-addon-stencil',
         '@storybook/addon-links',
         '@storybook/addon-essentials',
         '@storybook/addon-storysource',
         '@storybook/addon-actions',
-        {
-            name: 'storybook-addon-stencil',
-            options: {
-                stencilOptions: {},
-            },
-        },
         'storybook-dark-mode',
         '@chromatic-com/storybook',
     ],
